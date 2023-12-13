e09d8e6e45212360be9b4292e11ceca2e6e3d9ca
Mihaly Pirik <misipirik@gmail.com>
2023-12-13 09:11:17 +0100

    Add project files.

---
+++ Asztalfoglalas/Asztal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asztalfoglalas
+{
+    public class Asztal
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Megnevezes { get; set; }
+
+        [Required]
+        public int Ferohely { get; set; }
+    }
+}
